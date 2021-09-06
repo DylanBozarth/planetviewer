8a3b7ee65c88aec343dbbf7fd1855f6bad8e7b5a
dylan <dylanabozarth@gmail.com>
2021-09-05 19:12:17 -0700

    debug planetview

--- src/systems/sol/sol-planets/Earth.js
+++ src/systems/sol/sol-planets/Earth.js
@@ -3,16 +3,16 @@ export const Earth = () => {
     return (
         <div className="earthView container-fluid text-center">
             <div className="row">
-                <div className="col-lg-4"></div>
+                <div className="col-lg-4 sidePlanetView"></div>
                 
-                <div className="col-lg-8 planetWrapper">  <div className="text-center">
-            <div className="earthBall">
-              <div className="wrap">
+                <div className="col-lg-8 planetWrapper">  
+           
+              <div className="earthBall">
                 <div className="background"></div>
                 <div className="clouds"></div>
               </div>
-            </div>
-          </div></div>
+            
+          </div>
                 
                
             </div>
