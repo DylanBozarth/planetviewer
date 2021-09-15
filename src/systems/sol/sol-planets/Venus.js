d3ca1d1a2d99c05c46da5048974735816a3a16b9
dylan <dylanabozarth@gmail.com>
2021-09-15 13:40:17 -0700

    improved planetview

--- src/systems/sol/sol-planets/Venus.js
+++ src/systems/sol/sol-planets/Venus.js
@@ -1,5 +1,25 @@
+import { PlanetView } from "../../../components/planetview"
 export const Venus = () => {
     return (
-        <div>1</div>
+        <div className="earthView container-fluid text-center">
+        <div className="row">
+            <div className="col-lg-4 sidePlanetView"><PlanetView name="Venus"></PlanetView></div>
+            
+            <div className="col-lg-8 planetWrapper">  
+       
+          <div className="venusBall">
+           <div className="venusClouds"></div> 
+          </div>
+          
+      </div>
+            
+           
+        </div>
+        
+        
+        
+        
+        
+        </div>
     )
 }
