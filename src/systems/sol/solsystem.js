858981e9603b002a8d9aea629c51bb0a2d277c82
dylan <dylanabozarth@gmail.com>
2021-09-17 11:02:11 -0700

    all on same timescale

--- src/systems/sol/solsystem.js
+++ src/systems/sol/solsystem.js
@@ -2,6 +2,7 @@ import "./sol-system.css";
 import { gsap } from "gsap";
 import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { motion } from "framer-motion";
 export const MiniPlanet = (props) => {
   return (
     <NavLink to={`planets/${props.name}`}>
@@ -12,27 +13,74 @@ export const MiniPlanet = (props) => {
   );
 };
 
+function RandomNum() {
+    return Math.floor(Math.random() * 3 );
+  }
+  let timeArray = [1, 5, 9]
+  const pagetransition = {
+  duration: timeArray[RandomNum()],
+};
 export const SolSystem = () => {
   useEffect(() => {
-    
+    console.log(RandomNum())
   });
-
+const FadeIn = {
+  in: {
+    opacity: 1,
+    duration: timeArray[RandomNum()]
+  },
+  out: {
+    opacity: 0,
+  },
+};  
   return (
-    <div className="sol-system-master">
+    <motion.div  className="sol-system-master">
       <div className="sun"></div>
 
-     <MiniPlanet name="Mercury" className="mercuryMini"></MiniPlanet>
-      <MiniPlanet name="Venus" className="venusMini"></MiniPlanet>
-        <MiniPlanet name="Earth" className="earthMini"></MiniPlanet>
-        <MiniPlanet name="Mars" className="marsMini"></MiniPlanet>
+     <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} ><MiniPlanet name="Mercury"  className="mercuryMini"></MiniPlanet></motion.div>
+       <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} ><MiniPlanet name="Venus" className="venusMini"></MiniPlanet></motion.div>
+         <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} ><MiniPlanet name="Earth" className="earthMini"></MiniPlanet></motion.div>
+        <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} > <MiniPlanet name="Mars" className="marsMini"></MiniPlanet></motion.div>
         {/*<MiniPlanet name="Asteroid-Belt" className="asteroidBelt"></MiniPlanet> */}
-        <MiniPlanet name="Juipter" className="juipterMini"></MiniPlanet>
-        <MiniPlanet name="Saturn" className="saturnMini"></MiniPlanet>
-        <MiniPlanet name="Uranus" className="uranusMini"></MiniPlanet>
-        <MiniPlanet name="Neptune" className="neptuneMini"></MiniPlanet>
+        <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} ><MiniPlanet name="Juipter" className="juipterMini"></MiniPlanet></motion.div>
+         <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} ><MiniPlanet name="Saturn" className="saturnMini"></MiniPlanet></motion.div>
+        <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} > <MiniPlanet name="Uranus" className="uranusMini"></MiniPlanet></motion.div>
+        <motion.div initial="out"
+    animate="in"
+    exit="out"
+    transition={pagetransition}
+    variants={FadeIn} > <MiniPlanet name="Neptune" className="neptuneMini"></MiniPlanet></motion.div>
         
        
       
-    </div>
+    </motion.div>
   );
 };
