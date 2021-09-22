da42eebacd4784b74b84e3b3414b3b02b3a0c7d9
dylan <dylanabozarth@gmail.com>
2021-09-22 16:58:37 -0700

    working on star labels

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,16 +3,17 @@ import {useHover, useState} from 'react'
 
 import { NavLink } from "react-router-dom";
 export const Navigation = () => {
+    const [hiddenUI, setHiddenUI] = useState(false)
     const [Hovered, setHovered] = useState('')
     return (
         
-        <div className="navMaster">
-<div className="container">
+        <div className={hiddenUI ? 'hidden': 'navMaster' }>
+<div className="container-fluid">
 <div className="row">
 
-   <NavLink to="/interstellar"> <div className="navRow col-sm-4"> <p className={Hovered ? 'navTitle' : 'hidden'}>Interstellar Space</p><div className="navIcon"></div></div></NavLink>
+    <div className="navRow col-sm-4"><NavLink to="/interstellar"> <p className={Hovered ? 'navTitle' : 'hidden'}>Interstellar Space</p><div className="navIcon"></div></NavLink></div>
     <div className="navRow col-sm-4"></div>
-    <div className="navRow col-sm-4"></div>
+    <div className="navRow col-sm-4"><div className="navIcon text-center" onClick={() => setHiddenUI(true)}>Hide UI</div></div>
 </div>
 </div>
 
--- src/interstellar-view/interstellar.jsx
+++ src/interstellar-view/interstellar.jsx
@@ -113,7 +113,7 @@ export const Interstellar = (props) => {
   return (
     <motion.div variants={viewStar} exit="exit" className="interstellar-space">
       {/*} <div className="top-warp warp"></div> */}
-     <NavLink to="/"><div className="sol-system">Our Solar System</div></NavLink>
+     <NavLink to="/" className="sol-system"><div className="sol-system"></div></NavLink><p className="star-label">Our sun</p>
       <div className="random-placement">{exoStars}</div>
 
       {/*<div className="bottom-warp warp"></div> */}
