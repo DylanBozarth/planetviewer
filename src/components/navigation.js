ac3dbafb7e105c5e0863f1923f64ecafc1ebc28c
dylan <dylanabozarth@gmail.com>
2021-09-21 20:54:16 -0700

    hovering and flexing

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,17 @@
 import "./navigation.css"
-
+import {useHover, useState} from 'react'
 
 export const Navigation = () => {
+    const [Hovered, setHovered] = useState('')
     return (
+        
         <div className="navMaster">
 <div className="container">
 
 
-    <div className="navRow"> <div className="navIcon"></div><div className="navIcon"></div><div className="navIcon"></div></div>
-    <div className="navRow"><div className="navIcon"></div></div>
-    <div className="navRow"><div className="navIcon"></div></div>
+    <div className="navRow"> <p className={Hovered ? 'hidden' : 'navTitle'}>Interstellar Space</p><div className="navIcon"></div></div>
+    <div className="navRow"></div>
+    <div className="navRow"></div>
 </div>
 
 
