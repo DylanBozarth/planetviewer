e8856e1602322bfcbf8b45a27a0651d688916183
dylan <dylanabozarth@gmail.com>
2021-09-30 21:03:42 -0700

    nav and labels

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 work in progress 
 phase 2: other star systems and transitions plus great UI 
 1. exo planets are wonky 
-2. UI is  not pretty
-3. stars and planets wiht labels
 4. name undefeiend 
 
+phase 3: 
+1. better ui
+2. labels 
 
 side ideas: 
 1. asteroid belt 
--- src/interstellar-view/stars/star.jsx
+++ src/interstellar-view/stars/star.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 export const Star = (props) => {
   return (
     <NavLink to={`/${props.starName}?starType=${props.starType}?starName=${props.name}`}  >
-      <p className={`starLabel`}>{props.starName}</p>
+     {/*} <p className={`starLabel`}>{props.starName}</p> */}
       <div className={``} >
         <div className={`starBall ${props.starType}`}></div>
       </div>
